d1e73a668a1fd2bdb5f87c39d6e7cd8571e9090d
Babatunde Adeoti <babaolu98@gmail.com>
2024-09-17 09:10:19 +0100

    Awaiting multiple tasks as they complete

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Implements a function that waits for a random number of seconds """
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """ Waits for multiple tasks """
+    delays = [asyncio.create_task(wait_random(max_delay)) for x in range(n)]
+    results = []
+    for result in asyncio.as_completed(delays):
+        results.append(await result)
+    return results
